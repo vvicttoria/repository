6e78fcf0e184344d74330cb07b29c86a91b231b3
vvicttoria <121055927+vvicttoria@users.noreply.github.com>
2023-01-16 19:39:23 +0200

    feat(main): Master commit with base Django setup

---
+++ myfirst/myfirst/apps/articles/models.py
@@ -0,0 +1,24 @@
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('the title of the article', max_length = 200)
+    article_text = models.TextField('the text of the article')
+    pub_date = models.DateTimeField('date of publication')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days =7))
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    author_name = models.CharField('the name of the author', max_length = 50)
+    comment_text = models.CharField('comment text', max_length = 200)
+
+    def __str__(self):
+        return self.author_name
